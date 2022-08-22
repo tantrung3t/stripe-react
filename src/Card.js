214d39c9d37226d60527c874bd2cd9e305890c16
tantrung3t <tantrung.dmc@gmail.com>
2022-08-22 15:28:07 +0700

    update

--- src/Card.js
+++ src/Card.js
@@ -8,45 +8,62 @@ const PUBLIC_KEY = "pk_test_51LX0LLKBIaBSQ6dHZRldDqWEVWbN7kq1m8vzLUFwjrTh9LJ3hrq
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 export default function Card() {
-	return (
-		<Elements stripe={stripeTestPromise}>
-			<Payment />
-		</Elements>
-	)
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <Payment />
+        </Elements>
+    )
 }
 
-function Payment(){
+function Payment() {
 
     const elements = useElements()
     const stripe = useStripe()
 
-    const handlesubmit = async (e) =>{
+    const handlesubmit = async (e) => {
         e.preventDefault()
 
-        const response = await axios.post("http://localhost:8000/intents/", {
-                    amount: 1000,
+        const response = await axios.post("http://localhost:8000/setup-intent/", {
+            amount: 1000,
         })
         console.log("Created payment")
         console.log(response.data.client_secret)
+        
 
-        const confirmCardPayment = await stripe.confirmCardPayment(
+        // const confirmCardPayment = await stripe.confirmCardPayment(
+        //     response.data.client_secret, {
+        //         // payment_method: {
+        //         //     card: elements.getElement(CardElement)
+        //         // }
+        //         payment_method: "pm_1LZRZcKBIaBSQ6dHZ835E8v3"
+        //     }
+        // )
+        // console.log(confirmCardPayment)
+        // console.log(confirmCardPayment.paymentIntent.status)
+
+        
+        
+
+
+        const confirmCardSetup = await stripe.confirmCardSetup(
             response.data.client_secret, {
                 payment_method: {
-                    card: elements.getElement(CardElement)
-                }
-            }
-        )
-        console.log(confirmCardPayment.paymentIntent.status)
-
-    } 
-    return(
+                    card: elements.getElement(CardElement),
+                },
+            })
+            .then(function (result) {
+                console.log(result)
+            });
+        console.log(confirmCardSetup) 
+    }
+    return (
         <>
-        <h1>Card</h1>
-        <form onSubmit={handlesubmit}>
-            <label htmlFor="card-element">Card</label>
-            <CardElement id="card-element"/>
-            <button>Pay</button>
-        </form>
+            <h1>Card</h1>
+            <form onSubmit={handlesubmit}>
+                <label htmlFor="card-element">Card</label>
+                <CardElement id="card-element" />
+                <button>Pay</button>
+            </form>
         </>
     )
 }
